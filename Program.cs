300f44e01a026488c01bcc33d0ddbde39763d50d
Ihor Tymoshenko <91391033+Bjornheim@users.noreply.github.com>
2024-12-30 10:53:13 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите, пожалуйста, цифру: ");
+int userInput = int.Parse(Console.ReadLine());
+
+for (int i = 0; i <= userInput; ++i)
+{
+    Console.WriteLine(i);
+}
+
+for (int j = userInput; j >= 0; --j)
+{
+    Console.WriteLine(j);
+}
